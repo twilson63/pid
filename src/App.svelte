dc973bf7d05e40d501cf60bee4fa761ee4b83c8c
twilson63 <twilson63@hey.com>
2024-09-12 08:56:20 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,72 @@
+<script>
+  //let pid = 'ptCu-Un-3FF8sZ5zNMYg43zRgSYAGVkjz2Lb0HZmx2M'
+  let pid = 'xU9zFkq3X2ZQ6olwNVvr1vUWIjc3kXTWr7xKQD6dh10'
+  let servers = [] 
+
+  const getSU = (pid) => fetch(`https://su-router.ao-testnet.xyz/${pid}?limit=1000&from=${Date.now() - 60000}`).then(async res => {
+      let body = await res.json() 
+      let url = res.url
+      console.log(body)
+      return { body, url }
+    }) 
+  const getCU = (pid) => fetch(`https://cu.ao-testnet.xyz/dry-run?process-id=${pid}`, {
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      Target: pid,
+      Owner: pid, 
+      Tags: [
+        {name: 'Action', value: 'Info'}
+      ]
+    })
+  }).then(async res => {
+      let body = await res.json() 
+      let url = res.url
+
+      return { body, url }
+    }) 
+
+  // const getMU = (pid) => fetch(`https://mu.ao-testnet.xyz?debug=true&process=${pid}&message=KNRUNxMG2vOX_oT6FwMOiQDk9zo5xD4EWGPfYm7A7IU&page=1`).then(async res => {
+  //     let body = await res.json() 
+  //     let url = res.url
+
+  //     return { body, url }
+  //   }) 
+  
+  async function handleClick() {  
+    console.log('Submitted PID:', pid);
+    const res = await Promise.all([getSU(pid), getCU(pid)])
+    servers = res.map(({url}) => (new URL(url)).origin )
+  }
+</script>
+
+<main style="width: 600px;">
+
+  <h1>pid</h1>
+
+  <div class="card">
+    <p>Enter pid:</p>
+    <input bind:value={pid} type="text" id="pid" name="pid" />
+    <button on:click={handleClick}>Find</button>
+  </div>
+  <ol>
+    {#each servers as server}
+    <li style="text-align:left">{server}</li>
+    {/each}
+  </ol>
+
+</main>
+
+<style>
+input {
+  width: 100%;
+  border-radius: 0.375rem;
+  border-color: #e5e7eb;
+  padding-right: 2.5rem;
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  padding: .5rem;
+  margin-bottom: 1rem;
+}
+</style>
